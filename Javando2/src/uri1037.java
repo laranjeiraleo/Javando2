6c6cdf8b245db11c5db9ccbe9789752eb6c6f6bc
laranjeiraleo <leo_laranjeira@hotmail.com>
2023-02-02 22:07:22 -0300

    Intervalo

---
+++ Javando2/src/uri1037.java
@@ -0,0 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class uri1037 {
+
+	public static void main(String[] args) {
+	Locale.setDefault(Locale.US);
+	Scanner sc = new Scanner(System.in);
+	
+	double valor = sc.nextDouble();
+	
+	if (valor >= 0.00 && valor <= 25.00 ) {
+		System.out.println("Intervalo [0,25]");
+		
+	} 
+		
+	else if (valor > 25.00 && valor <= 50.00) {
+		System.out.println("Intervalo (25,50]");
+		
+	}
+	
+	else if (valor > 50.00 && valor <= 75.00) {
+		System.out.println("Intervalo (50,75]");	
+	}
+	
+	else if (valor > 75.00 && valor <= 100.00) {
+		System.out.println("Intervalo (75,100]");
+	}
+	
+	
+	
+	sc.close();
+	}
+
+}
