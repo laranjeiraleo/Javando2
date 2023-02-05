70db8fc5e1a8b847c439c6d5e2986e8cdbfcb0a4
laranjeiraleo <leo_laranjeira@hotmail.com>
2023-02-05 15:48:29 -0300

    Tipos de Triângulos

---
+++ Javando2/src/uri1045.java
@@ -0,0 +1,61 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class uri1045 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double A, B, C, A2, BC, maior;
+		
+		A = sc.nextDouble();
+		B = sc.nextDouble();
+		C = sc.nextDouble();
+		
+		if (B > A) {
+			maior = A;
+			A = B;
+			B = maior;
+			
+		}
+		
+		if (C > A) {
+			maior = A;
+			A = C;
+			C = maior;
+			
+		}
+		
+		A2 = Math.pow(A, 2);
+		BC = Math.pow(B, 2) + Math.pow(C, 2);
+		
+		if (A >= B + C) {
+			System.out.println("NAO FORMA TRIANGULO");
+		}
+		
+		else {
+			if (A2 == BC) {
+				System.out.println("TRIANGULO RETANGULO");
+			}
+			else if (A2 > BC) {
+				System.out.println("TRIANGULO OBTUSANGULO");
+			}
+			else if (A2 < BC) {
+				System.out.println("TRIANGULO ACUTANGULO");
+			}
+		}	
+		
+		 	if (A == B && B == C) {
+			 System.out.println("TRIANGULO EQUILATERO");
+		 	}
+		 	else if (A == B || B == C || C == A) {
+			 System.out.println("TRIANGULO ISOSCELES");
+		 	}
+		
+		sc.close();
+	
+
+	}
+
+}
